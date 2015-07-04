62145e6f5d96f1805a218426a4a5494c8437fc40
nozomukubota <nozomu.k@cnxt.jp>
2015-07-04 23:23:32 +0000

    validation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,16 +2,23 @@ class MessagesController < ApplicationController
   def index
     #Messageを全て取得する
     @messages=Message.all
+    @message=Message.new
   end
 
   ## ここから追記
   def create
     @message = Message.new(message_params)
-    @message.save
-    redirect_to root_path , notice: 'メッセージを保存しました'
-  end
+    if @message.save
+      redirect_to root_path , notice: 'メッセージを保存しました'
+    else
+      # メッセージが保存できなかった時
+      @messages = Message.all
+      flash.now[:alert] = "メッセージの保存に失敗しました。"
+      render 'index'
+    end
 
-  private
+  end
+private
   def message_params
     params.require(:message).permit(:name, :body)
   end
